3a293153d9bf6119d864adccdabaa39434cdaafc
akanSWE <a.akanSWE@gamil.com>
2024-06-29 05:26:17 +0100

    fix: puppet-lint arrow alignment warnings

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file school in /tmp, set it metadata and inserts a text
 
-file {'/tmp/school':
-  ensure => 'present',
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744',
+file { '/tmp/school':
+  ensure  => 'present',
+    owner => 'www-data',
+    group => 'www-data',
+    mode  => '0744',
   content => 'I love Puppet'
 }
