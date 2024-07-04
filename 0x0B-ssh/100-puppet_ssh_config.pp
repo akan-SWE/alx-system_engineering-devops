72515f8b79c89980c8473f264fbffdfbc35b353d
akanSWE <a.akanSWE@gamil.com>
2024-07-04 05:45:27 +0100

    Update manifest

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,28 @@
 # This Puppet manifest appends a configuration block to the global SSH configuration file.
 # The configuration block sets up an alias "schoolserver" for the host with the IP address 100.26.173.36,
 # specifies the identity file to use when connecting to the host, and disables password authentication.
-exec {'ssh_config':
-  command => "/bin/echo -e '
-    Host schoolserver
-        HostName 100.26.173.36
-        user ubuntu
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no' >> /etc/ssh/ssh_config",
-  path    => ['/bin/', '/usr/bin/'],
-  unless  => '/bin/grep -q "Host schoolserver" /etc/ssh/ssh_config'
+
+# Include the stdlib module
+include stdlib
+
+# Ensure the SSH client configuration directory exists
+file { '~/.ssh':
+  ensure => directory,
+}
+
+# Ensure the SSH client configuration file exists
+file { '~/.ssh/school':
+  ensure => file,
+}
+
+# Configure SSH client to use the private key
+file_line { 'Declare identity file':
+  path  => '~/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+}
+
+# Configure SSH client to refuse password authentication
+file_line { 'Turn off passwd auth':
+  path  => '~/.ssh/config',
+  line  => 'PasswordAuthentication no',
 }
